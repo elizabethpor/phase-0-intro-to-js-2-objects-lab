c34a7e9f6c90c8b10185e8acd4938a89d2db204a
Elizabeth <elizabeth.por@gmail.com>
2022-01-13 19:48:49 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+const employee = {
+    name: "Elizabeth",
+    street: "5327 Itaska",
+}
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const employeeUpdated = {...employee};
+    employeeUpdated[key] = value;
+    return employeeUpdated;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, key) {
+    const employeeWithoutKey = {...employee};
+    delete employeeWithoutKey[key];
+    return employeeWithoutKey;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+}
